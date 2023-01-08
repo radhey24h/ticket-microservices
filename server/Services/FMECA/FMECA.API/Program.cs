6c5980d507274b3c49512e17f3fa4adb2577ee14
radhey24h <radhey24h@gmail.com>
2023-01-08 11:33:20 +0530

    changes

--- server/Services/FMECA/FMECA.API/Program.cs
+++ server/Services/FMECA/FMECA.API/Program.cs
@@ -1,7 +1,6 @@
 
 using FMECA.Application;
 using FMECA.Infrastructure;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -10,10 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FMECA.API", Version = "v1" });
-//});
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
@@ -24,12 +19,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseDeveloperExceptionPage();
-//    app.UseSwagger();
-//    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FMECA.API v1"));
-//}
 
 app.UseHttpsRedirection();
 
